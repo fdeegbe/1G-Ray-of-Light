a8cab71fa265bb66134e91496b4d7493529ce08c
fdeegbe1 <87954337+fdeegbe@users.noreply.github.com>
2023-10-01 22:14:04 +0000

    ...

--- Recycling/MITM.sh
+++
@@ -1,3 +0,0 @@
-# This script will take in the name of the contianer, the container IP, and the port and
-# start the MITM server in background mode using forever. It will set up nat rules and
-# firewalls as well.
--- Recycling/recycle_delete.sh
+++
@@ -1,3 +0,0 @@
-# This script will delete the current instances of the containers in preparation
-# for new honeypot systems to be created.
-
