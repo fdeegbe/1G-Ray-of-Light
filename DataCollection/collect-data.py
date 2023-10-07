abb3a9f072b8526806dfc21a477604fd5ad935af
John Hooker <hookerj100@gmail.com>
2023-10-07 09:28:22 -0400

    starting data collection

---
+++ DataCollection/collect-data.py
@@ -0,0 +1,11 @@
+import os
+from datetime import date
+from datetime import timedelta
+
+today = date.today()
+yesterday = today - timedelta(days = 1)
+
+# swap yesterday for today when done with testing
+for filename in os.listdir("/MITM/mitm_logs/" + today):
+    print("Filename: " + str(filename))
+    
