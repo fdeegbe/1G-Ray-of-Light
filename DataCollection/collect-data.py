41bee038ca2cb3368516852cab45238154207a29
John Hooker <hookerj100@gmail.com>
2023-10-07 09:31:39 -0400

    str for date

--- DataCollection/collect-data.py
+++ DataCollection/collect-data.py
@@ -6,6 +6,6 @@ today = date.today()
 yesterday = today - timedelta(days = 1)
 
 # swap yesterday for today when done with testing
-for filename in os.listdir("/MITM/mitm_logs/" + today):
+for filename in os.listdir("/MITM/mitm_logs/" + str(today)):
     print("Filename: " + str(filename))
     
