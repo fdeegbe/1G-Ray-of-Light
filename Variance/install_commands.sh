9a60c0384a941b71e353aaea038e83b64b0cb45e
Andrew Yang <andrewyang1015@gmail.com>
2023-10-10 19:42:02 -0400

    HACS200 fixed installation script for commands

--- Variance/install_commands.sh
+++ Variance/install_commands.sh
@@ -2,13 +2,16 @@
 
 # We will be tracking the following commands: tar, gzip/gunzip, bzip2/bunzip2, zip/unzip
 # rar/unrar, wget, curl, scp, rsync, ftp/sftp, git, aria2, axel
+
+# This script assumes that containers have been created and started
 if [ $# -ne 1 ]
 then
-    echo "Usage: pass in the container name you retard"
+    echo "Usage: container name"
 else
-    container_name = $1
+    container_name=$1
     sudo lxc-attach -n $container_name -- bash -c "sudo apt-get update && \
                                                    sudo apt-get install wget curl zip unzip \
                                                    ftp git unrar aria2 axel"
-    echo "Successfully installed the following commands"
+    echo "Successfully installed the following commands: tar, gzip/gunzip, bzip2/bunzip2, zip/unzip\
+    rar/unrar, wget, curl, scp, rsync, ftp/sftp, git, aria2, axel"
 fi
