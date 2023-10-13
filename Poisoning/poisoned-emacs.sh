7589ae193ccfc4bfc26c683ba75e6df7e08f2c8f
Andrew <ayang03@terpmail.umd.edu>
2023-10-13 14:43:39 -0400

    added template for text-editor and sudo commands

--- Poisoning/poisoned-emacs.sh
+++ Poisoning/poisoned-emacs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-echo "Sampel poisoned emacs"
-
+#/usr/bin/real-emacs "$@" >> /var/log/.downloads/emacs_log 2> /dev/null
+/usr/bin/real-emacs "$@"
+echo "You have just used emacs, you will die in 30 seconds"
