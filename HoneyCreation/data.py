1a41642af2f2107fab4dbf762081462805eec808
fdeegbe1 <87954337+fdeegbe@users.noreply.github.com>
2023-10-15 21:04:31 +0000

    Added file generation method

--- HoneyCreation/data.py
+++
@@ -1,8 +0,0 @@
-HEADER = """\
-                                   UNIVERSITY OF MARYLAND
-                                        COLLEGE PARK
-                                   Office of the Registrar
-                                   College Park, MD 20742
-                                   UNOFFICIAL TRANSCRIPT
-                                FOR ADVISING PURPOSES ONLY 
-                                """
