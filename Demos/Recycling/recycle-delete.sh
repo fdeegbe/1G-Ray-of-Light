f2be634c6c42dd41c75ceb6cc1ace4ee4ee07b90
fdeegbe <87954337+fdeegbe@users.noreply.github.com>
2024-01-08 12:45:44 -0500

    Delete Demos directory
    Prepare documentation

--- Demos/Recycling/recycle-delete.sh
+++
@@ -1,7 +0,0 @@
-# This script will stop and delete the 4 running containers in preparation
-# for the creation of the new systems
-
-sudo docker rm --force container1
-sudo docker rm --force container2
-sudo docker rm --force container3
-sudo docker rm --force container4
